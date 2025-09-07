4ea7f8bf3b5e1f0f012aca548a0f658c6329d4b7
locser <>
2025-09-07 17:34:53 +0700

    07/09/25

--- design/algori/locph/leetcode.september.ts
+++ design/algori/locph/leetcode.september.ts
@@ -1,114 +1,196 @@
 /**
- * Example 1:
+https://leetcode.com/problems/surface-area-of-3d-shapes/submissions/1762541004/?envType=problem-list-v2&envId=2cf6vun6
+    mỗi một cột có thể tính bằng phương thức 2+ v *4 (với 2 là 2 mặt đáy và v là số lượng stack của cột)
 
-Input: jewels = "aA", stones = "aAAbbbb"
-Output: 3
-Example 2:
+    đi theo bên trái (ví dụ 2 có 1 ở bên trái), bên trên (ví dụ 3 có 1 ở phía trên, theo exmaple 1) thì với mỗi stack giáp nhau trừ đi 2 mặt, do 2 mặt này tiếp xúc nên bị trừ
 
-Input: jewels = "z", stones = "ZZ"
-Output: 0
  */
-function numJewelsInStones(jewels: string, stones: string): number {
+
+function surfaceArea(grid: number[][]): number {
+  const n = grid.length;
+
+  if (n == 0) return 0;
   let count = 0;
 
-  const mapJewel = {};
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      const v = grid[row][col];
 
-  for (let i = 0; i < jewels.length; i++) {
-    mapJewel[jewels[i]] = 1;
-  }
+      // trường hợp nếu là 0 thì bỏ qua luôn
+      if (v > 0) {
+        count = count + (2 + 4 * v); // trường hợp đẹp nhất
 
-  for (let i = 0; i < stones.length; i++) {
-    if (mapJewel[stones[i]]) {
-      count++;
-    }
-  }
-  return count;
-}
-console.log(numJewelsInStones("aA", "aAAbbbb"));
-console.log(numJewelsInStones("z", "ZZ"));
+        // nếu bên trái có tiếp giáp col > 0, có nghĩa là không phải cột đầu tiên
 
-//https://leetcode.com/problems/make-a-square-with-the-same-color/?envType=problem-list-v2&envId=2cf6vun6
-function canMakeSquare(grid: string[][]): boolean {
-  const mapColor = {};
-
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      mapColor[`${i},${j}`] = grid[i][j] === "B" ? 1 : 0;
-    }
-  }
-
-  const a = [0, 1, 3, 4];
-
-  for (let i = 0; i < grid.length - 1; i++) {
-    for (let j = 0; j < grid[i].length - 1; j++) {
-      const sum =
-        mapColor[`${i},${j}`] +
-        mapColor[`${i},${j + 1}`] +
-        mapColor[`${i + 1},${j}`] +
-        mapColor[`${i + 1},${j + 1}`];
-
-      if (sum !== 2) {
-        return true;
+        if (col > 0) {
+          // lấy ra số lượng cột trùng nhau
+          count = count - Math.min(v, grid[row][col - 1]) * 2;
+        }
+        // nếu bên phải có tiếp giáp
+        if (row > 0) {
+          // lấy ra số lượng cột trùng nhau
+          count = count - Math.min(v, grid[row - 1][col]) * 2;
+        }
       }
     }
   }
 
-  return false;
+  return count;
 }
 
-console.log(
-  canMakeSquare([
-    ["B", "W", "B"],
-    ["B", "W", "W"],
-    ["B", "W", "B"],
-  ])
-);
-console.log(
-  canMakeSquare([
-    ["B", "W", "B"],
-    ["W", "B", "W"],
-    ["B", "W", "B"],
-  ])
-);
-console.log(
-  canMakeSquare([
-    ["B", "W", "B"],
-    ["B", "W", "W"],
-    ["B", "W", "W"],
-  ])
-);
+/**
+    https://leetcode.com/problems/number-of-even-and-odd-bits/?envType=problem-list-v2&envId=2cf6vun6
 
-//https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/?envType=problem-list-v2&envId=2cf6vun6
-function makeEqual(words: string[]): boolean {
-  /**
-        - dịch chuyển các kí tự ở các string sao cho tất cả các string đều bằng nhau. nếu có thể trả về true
-        - tiếp cận: các string bằng nhau thì số các kí tự của 1 loại trong words cần phải được chia đều và không dư
-        - nếu dư sẽ là không bằng nhau return false
-        - ví dụ words = ["abc","aabc","bc"] thì có 3a, 3b,3c chia đều được cho số phần tử là 3
-     */
+    - ta có thể dùng toString(2) => để giải 1 số thành số nhị phân
+    - toString có thể nhận một tham số radix (hệ cơ số) để chuyển đổi số thành chuỗi trong hệ cơ số tương ứng. Khi bạn gọi a.toString(2), bạn đang yêu cầu JavaScript chuyển đổi số a (trong trường hợp này là 50) thành chuỗi nhị phân.
+ */
 
-  const characterCount: Record<string, number> = {};
-
-  for (const word of words) {
-    // Count each character in the current word
-    for (const character of word) {
-      characterCount[character] = (characterCount[character] || 0) + 1;
+function evenOddBit(n: number): number[] {
+  const result: number[] = [];
+  const binary = n.toString(2);
+  let even = 0,
+    odd = 0;
+  const leng = binary.length;
+  for (let i = 0; i < leng; i++) {
+    if (binary[i] === "1") {
+      if ((leng - i - 1) % 2 == 0) {
+        even++;
+      } else {
+        odd++;
+      }
     }
   }
-
-  //  tất cả các phần tử trong map đều phải có value chia hết cho words.length
-  const leng = words.length;
-
-  return Object.values(characterCount).every(
-    (count: any) => count % leng === 0
-  );
+  return [even, odd];
 }
-console.log("makeEqual");
-console.log(makeEqual(["abc", "aabc", "bc"]));
-console.log(makeEqual(["abc", "aabc", "bc", "bcc"]));
-console.log(makeEqual(["abc", "aabc", "bc", "bcc", "ccc"]));
-console.log(makeEqual(["abc", "aabc", "bc", "bcc", "ccc", "cccc"]));
-console.log(makeEqual(["abc", "aabc", "bc", "bcc", "ccc", "cccc", "ccccc"]));
-console.log(
-  makeEqual(["abc", "aabc", "bc", "bcc", "ccc", "cccc", "ccccc", "cccccc"])
-);
+
+// https://leetcode.com/problems/number-of-changing-keys/?envType=problem-list-v2&envId=2cf6vun6
+/**
+    xem có bao nhiêu sự thay đổi nút khi gõ nhập chuỗi
+    các phím CapsLock hay Shift không được tính
+    ví dụ, nếu aA thì chỉ là 1 nút a không hề có sự thay đổi nút nào cả
+    ví dụ ABCa, thì có 3, A-> B, B-> C C->a
+ */
+function countKeyChanges(s: string): number {
+  let last: string = "";
+  let count = 0;
+  s.toLowerCase()
+    .split("")
+    .forEach((char: string) => {
+      if (last !== char) {
+        count++;
+        last = char;
+      }
+    });
+
+  return count - 1;
+}
+
+console.log(countKeyChanges("aAbBcC"));
+console.log(countKeyChanges("AaAaAaaA"));
+
+// /**
+//  * Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+//  */
+// function numJewelsInStones(jewels: string, stones: string): number {
+//   let count = 0;
+
+//   const mapJewel = {};
+
+//   for (let i = 0; i < jewels.length; i++) {
+//     mapJewel[jewels[i]] = 1;
+//   }
+
+//   for (let i = 0; i < stones.length; i++) {
+//     if (mapJewel[stones[i]]) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(numJewelsInStones("aA", "aAAbbbb"));
+// console.log(numJewelsInStones("z", "ZZ"));
+
+// //https://leetcode.com/problems/make-a-square-with-the-same-color/?envType=problem-list-v2&envId=2cf6vun6
+// function canMakeSquare(grid: string[][]): boolean {
+//   const mapColor = {};
+
+//   for (let i = 0; i < grid.length; i++) {
+//     for (let j = 0; j < grid[i].length; j++) {
+//       mapColor[`${i},${j}`] = grid[i][j] === "B" ? 1 : 0;
+//     }
+//   }
+
+//   const a = [0, 1, 3, 4];
+
+//   for (let i = 0; i < grid.length - 1; i++) {
+//     for (let j = 0; j < grid[i].length - 1; j++) {
+//       const sum =
+//         mapColor[`${i},${j}`] +
+//         mapColor[`${i},${j + 1}`] +
+//         mapColor[`${i + 1},${j}`] +
+//         mapColor[`${i + 1},${j + 1}`];
+
+//       if (sum !== 2) {
+//         return true;
+//       }
+//     }
+//   }
+
+//   return false;
+// }
+
+// console.log(
+//   canMakeSquare([
+//     ["B", "W", "B"],
+//     ["B", "W", "W"],
+//     ["B", "W", "B"],
+//   ])
+// );
+// console.log(
+//   canMakeSquare([
+//     ["B", "W", "B"],
+//     ["W", "B", "W"],
+//     ["B", "W", "B"],
+//   ])
+// );
+// console.log(
+//   canMakeSquare([
+//     ["B", "W", "B"],
+//     ["B", "W", "W"],
+//     ["B", "W", "W"],
+//   ])
+// );
+
+// //https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/?envType=problem-list-v2&envId=2cf6vun6
+// function makeEqual(words: string[]): boolean {
+//   /**
+//         - dịch chuyển các kí tự ở các string sao cho tất cả các string đều bằng nhau. nếu có thể trả về true
+//         - tiếp cận: các string bằng nhau thì số các kí tự của 1 loại trong words cần phải được chia đều và không dư
+//         - nếu dư sẽ là không bằng nhau return false
+//         - ví dụ words = ["abc","aabc","bc"] thì có 3a, 3b,3c chia đều được cho số phần tử là 3
+//      */
+
+//   const characterCount: Record<string, number> = {};
+
+//   for (const word of words) {
+//     // Count each character in the current word
+//     for (const character of word) {
+//       characterCount[character] = (characterCount[character] || 0) + 1;
+//     }
+//   }
+
+//   //  tất cả các phần tử trong map đều phải có value chia hết cho words.length
+//   const leng = words.length;
+
+//   return Object.values(characterCount).every(
+//     (count: any) => count % leng === 0
+//   );
+// }
+// console.log("makeEqual");
